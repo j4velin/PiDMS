f81cc8cff5a11201343cac457c5f17dfa8296c8a
Thomas Hoffmann <j4velin@users.noreply.github.com>
2020-09-20 11:46:11 +0200

    Add error handling

--- scanserver.py
+++ scanserver.py
@@ -2,14 +2,22 @@
 
 from flask import Flask
 from flask import request
-import os
+import subprocess
+import datetime
 
 http_server = Flask(__name__)
 
 @http_server.route('/<int:count>', methods=['GET'])
 def scan(count):
-    os.system('/home/pi/scan.sh ' + str(count) + ' 2>&1 | tee /tmp/scan.log')
-    return "scanning " + str(count) + " pages..."
+    scan = subprocess.Popen('/home/pi/scan.sh ' + str(count) + ' 2>&1', stdout=subprocess.PIPE, shell=True)
+    out, err = scan.communicate()
+    if scan.returncode == 0:
+        return "scanned " + str(count) + " pages"
+    else:
+        with open('/tmp/scan.log', 'a') as log:
+            log.write(str(datetime.datetime.now()) + '\n')
+            log.write(out + '\n')
+        return "error trying to scan, error code: " + str(scan.returncode) + "\n" + out, 500
 
 if __name__ == '__main__':
     http_server.run(host='0.0.0.0', port=8080)
